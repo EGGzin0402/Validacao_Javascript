e1fb7c8701066abc2b677dfd032793ccc96a6b97
Eliel Godoy <eliel.godoy@etec.sp.gov.br>
2023-09-15 17:29:10 -0300

    CEP automático

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputTel = document.getElementById("telefone");
 const inputCel = document.getElementById("celular");
 const inputRG = document.getElementById("rg");
 const inputCEP = document.getElementById("cep");
+const inputEstado = document.getElementById("estado");
 
 inputCPF.addEventListener("keypress", FormatarCPF);
 inputTel.addEventListener("keypress", FormatarTel);
@@ -47,7 +48,7 @@ function FormatarCel() {
   if (inputlength == 3) {
     inputCel.value += ")";
   }
-  if (inputlength == 8) {
+  if (inputlength == 9) {
     inputCel.value += "-";
   }
 }
@@ -143,7 +144,8 @@ function SomenteNumero(e) {
     }
 }
 
-function ValidacaoGeral() {
+function NotNull() {
+
     let inputs = document.querySelectorAll("input");
     if (inputs[0].value == "") {
         console.log("Preencha o campo "+inputs[0].id);
@@ -163,15 +165,91 @@ function ValidacaoGeral() {
     if (inputs[5].value == "") {
         console.log("Preencha o campo "+inputs[5].id);
     }
+    if (inputEstado.value == ""){
+        console.log("Preencha o campo "+inputEstado.id);
+    }
     if (inputs[6].value == "") {
         console.log("Preencha o campo "+inputs[6].id);
     }
     if (inputs[7].value == "") {
         console.log("Preencha o campo "+inputs[7].id);
-    }else{
-        ValidarCPF();
     }
     if (inputs[8].value == "") {
         console.log("Preencha o campo "+inputs[8].id);
     }
+    
 }
+
+function ValidacaoGeral() {
+
+    NotNull();
+    
+    ValidarCPF();
+    
+}
+
+
+$(document).ready(function() {
+
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("#endereco").val("");
+        $("#bairro").val("");
+        $("#cidade").val("");
+        $("#estado").val("");
+    }
+    
+    //Quando o campo cep perde o foco.
+    $("#cep").blur(
+        function ConsultaCEP() {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("#endereco").val("...");
+                $("#bairro").val("...");
+                $("#cidade").val("...");
+                $("#uf").val("...");
+                $("#ibge").val("...");
+
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("#endereco").val(dados.logradouro);
+                        $("#bairro").val(dados.bairro);
+                        $("#cidade").val(dados.localidade);
+                        $("#estado").val(dados.uf);
+                        $("#ibge").val(dados.ibge);
+                        console.log("CEP encontrado.");
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        console.log("CEP não encontrado.");
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });
+});
